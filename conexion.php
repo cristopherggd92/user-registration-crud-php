e6f9d3a92208df4138090a438485efea4b255c44
Cristopher Gaytán <37095417+cristopherggd92@users.noreply.github.com>
2026-08-25 13:39:57 -0600

    Add database connection

---
+++ conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$base_datos = "actividad_2";
+
+$conexion = new mysqli(
+    $servidor,
+    $usuario,
+    $password,
+    $base_datos
+);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+$conexion->set_charset("utf8mb4");
